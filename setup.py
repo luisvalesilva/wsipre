0070b655137610f7672e04c0132943b69497b343
Luis Vale Silva <luisvalesilva@gmail.com>
2018-12-20 09:25:36 +0100

    increase version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wsipre",
-    version="0.1.0",
+    version="0.1.1",
     author="Luis A. Vale Silva",
     author_email="luisvalesilva@gmail.com",
     description="Whole-slide image preprocessing",
